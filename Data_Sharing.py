d59be8d5e08f0d0b8b6f41e5ec1c4b6333f104b7
Devkumar Banerjee <45304559+aptitudepi@users.noreply.github.com>
2024-06-28 16:23:32 -0500

    Update Data_Sharing.py to set page size globally

--- Data_Sharing.py
+++ Data_Sharing.py
@@ -13,6 +13,8 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+# Set page layout to "wide" to make plots larger
+st.set_page_config(layout="wide")
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
