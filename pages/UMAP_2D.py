9e9365d2fee5d894d253514287186e1b15b17b6e
aptitudepi <45304559+aptitudepi@users.noreply.github.com>
2024-07-28 14:40:47 -0500

    Banerjee, Devkumar - Add search/filter to streamlit UMAP, add stuff to Data Summary page

--- pages/UMAP_2D.py
+++ pages/UMAP_2D.py
@@ -8,12 +8,94 @@ from sklearn.decomposition import PCA
 from sklearn import cluster
 import hdbscan
 import pandas as pd
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
 from scipy.stats import median_abs_deviation
 import plotly.express as px
 
 st.set_page_config(layout="wide")
 
-# Define (cached) helper functions for better performance
+# Define (mostly cached) helper functions for better performance
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            left.write("↳")
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    _min,
+                    _max,
+                    (_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].str.contains(user_text_input)]
+
+    return df
+
 @st.cache_data
 def Preprocess(filepath, num_genes):
 	df = pd.read_csv("data/logcpm.csv")
@@ -37,9 +119,8 @@ def UMAP(top_genes):
 	# Replace transposed columns with axes labels
         return pd.DataFrame(UMAP_model().fit_transform(top_genes), columns=["UMAP1", "UMAP2"])
 
-@st.cache_data
 def Metadata():
-	return pd.read_csv("data/metadata.csv")
+	return filter_dataframe(pd.read_csv("data/metadata.csv"))
 
 #def Label():
 	#if (algo == "HDBScan"):
@@ -64,13 +145,16 @@ def main():
 	# Perform the Data Preprocessing
 	top_genes = Preprocess("data/logcpm.csv", num_genes)
 
+	# Set metadata here
+	metadf = Metadata()
+
 	# Apply UMAP,combine 2D embedding coordinates with metadata to allow access in the plotting function
-	coords = pd.concat([UMAP(top_genes), Metadata()], axis=1)
+	coords = pd.concat([UMAP(top_genes), metadf], axis=1)
 
 	# Cluster data for plot colors
 	# clusters = Metadata()["Genotype"]
 	# Create the UMAP plot
-	fig = Plot(coords,Metadata()["Genotype"])
+	fig = Plot(coords,metadf["Genotype"])
 
 	# Display the plot in Streamlit
 	st.plotly_chart(fig, use_container_width=True)
