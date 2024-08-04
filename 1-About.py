2b4b1f83842ec9d430f21b1e322d4865ff0a9956
aptitudepi <45304559+aptitudepi@users.noreply.github.com>
2024-08-03 20:22:03 -0500

    Banerjee, Devkumar - Replace br with actual line break

--- 1-About.py
+++ 1-About.py
@@ -14,8 +14,8 @@ def main():
 	"""
 	# Pheochromocytoma and Paraganglioma Genomic Portal
 
-	Pheochromocytoma and paraganglioma (PCPG) are rare neuroendocrine tumors with limited genomic data resources, hindering research progress and therapeutic development. We have profiled 83 PCPG tumors using both bulk and single-cell/nuclei datasets in collaboration between UT Health San Antonio (UTHSA) and University of California (UCLA). This transcriptomic data is diverse, including bulk RNA sequencing data of primary tumors and organoids, as well as pseudobulk data of the single-cell/nuclei RNA sequencing samples. We have also utilizied the publicaly available PCPG transctiptomic data from the cancer genome atlas (TCGA).
-	<br />
+	Pheochromocytoma and paraganglioma (PCPG) are rare neuroendocrine tumors with limited genomic data resources, hindering research progress and therapeutic development. We have profiled 83 PCPG tumors using both bulk and single-cell/nuclei datasets in collaboration between UT Health San Antonio (UTHSA) and University of California (UCLA). This transcriptomic data is diverse, including bulk RNA sequencing data of primary tumors and organoids, as well as pseudobulk data of the single-cell/nuclei RNA sequencing samples. We have also utilizied the publicaly available PCPG transctiptomic data from the cancer genome atlas (TCGA).  \n
+
 	We have created this portal to share this transcriptomic data of PCPG tumors with the community. It allows users to explore gene expression patterns, visualize sample clusters, and analyze relationships between genotype and transcriptome clusters of individual samples.
 
 	"""
